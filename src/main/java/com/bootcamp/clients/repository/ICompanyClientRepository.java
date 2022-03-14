a298a9328244b97c00211bbac5e7ad4b1a6a4b8f
gregori <grego07h@gmail.com>
2022-03-14 09:49:57 -0500

    ms-client respository complete

--- src/main/java/com/bootcamp/clients/repository/ICompanyClientRepository.java
+++ src/main/java/com/bootcamp/clients/repository/ICompanyClientRepository.java
@@ -4,6 +4,7 @@ import com.bootcamp.clients.entity.CompanyClient;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface ICompanyClientRepository  extends ReactiveMongoRepository<CompanyClient, String> {
 
 }
