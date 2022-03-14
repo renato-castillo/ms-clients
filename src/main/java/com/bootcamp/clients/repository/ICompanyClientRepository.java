875d40095b46de56b2d7f02139e16a21d1239239
renato-castillo <renatocastillopingo@gmail.com>
2022-03-14 12:19:10 -0500

    Edited for bootstrap.yml

--- src/main/java/com/bootcamp/clients/repository/ICompanyClientRepository.java
+++ src/main/java/com/bootcamp/clients/repository/ICompanyClientRepository.java
@@ -2,6 +2,7 @@ package com.bootcamp.clients.repository;
 
 import com.bootcamp.clients.entity.CompanyClient;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 @Repository
