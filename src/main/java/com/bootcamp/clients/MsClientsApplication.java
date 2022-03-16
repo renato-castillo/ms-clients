9463aad5aed7ffb761d88522360a621f33eb8c94
renato-castillo <renatocastillopingo@gmail.com>
2022-03-16 13:30:48 -0500

    Added eureka client

--- src/main/java/com/bootcamp/clients/MsClientsApplication.java
+++ src/main/java/com/bootcamp/clients/MsClientsApplication.java
@@ -3,10 +3,12 @@ package com.bootcamp.clients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EntityScan(basePackages = "com.bootcamp.clients")
 @SpringBootApplication(scanBasePackages = "com.bootcamp.clients")
+@EnableEurekaClient
 public class MsClientsApplication {
 
     public static void main(String[] args) {
