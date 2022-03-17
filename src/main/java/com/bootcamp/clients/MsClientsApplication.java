074c3104a2803ebd11a95f9a6c606b674fab6a0b
renato-castillo <renatocastillopingo@gmail.com>
2022-03-17 15:40:00 -0500

    Added logic max per client in companny account

--- src/main/java/com/bootcamp/clients/MsClientsApplication.java
+++ src/main/java/com/bootcamp/clients/MsClientsApplication.java
@@ -3,12 +3,13 @@ package com.bootcamp.clients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EntityScan(basePackages = "com.bootcamp.clients")
 @SpringBootApplication(scanBasePackages = "com.bootcamp.clients")
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class MsClientsApplication {
 
     public static void main(String[] args) {
